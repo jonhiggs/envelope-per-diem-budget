e34d66791e4534c88868439ab93a24f7ecccd46e
Jon Higgs <jhiggs@eml.cc>
2016-08-26 12:45:17 +1000

    Make the calendar class simpler

--- lib/budget/calendar.rb
+++ lib/budget/calendar.rb
@@ -9,6 +9,15 @@ module Budget
       @payday = payday
     end
 
+    def days
+      (start_of_month..end_of_month).to_a
+    end
+
+    def bonus_days
+      days.select { |d| d.wday == 5 }
+    end
+
+    private
     def payday relative_month=0
       relative_month += 1 if @day.day > @payday
       date = @day + relative_month.months
@@ -17,36 +26,13 @@ module Budget
       Date.parse("#{year}/#{month}/#{@payday}")
     end
 
-    def next_payday
-      payday(0)
-    end
-
-    def last_payday
+    def start_of_month
       payday(-1)
     end
 
-    def days
-      (last_payday..next_payday-1).to_a
+    def end_of_month
+      payday(0) - 1.day
     end
 
-    def remaining_days
-      (@day+1..next_payday-1).to_a
-    end
-
-    def elapsed_days
-      (last_payday..@day).to_a
-    end
-
-    def bonus_days
-      (last_payday..next_payday).to_a.select { |d| d.wday == 5 }
-    end
-
-    def remaining_bonuses
-      (@day..next_payday).to_a.select { |d| d.wday == 5 }
-    end
-
-    def bonus_day?
-      @day.friday?
-    end
   end
 end
