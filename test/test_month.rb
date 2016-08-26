d45fb184f1a072fc8c2f005d8e06017b92215754
Jon Higgs <jhiggs@eml.cc>
2016-08-26 13:50:07 +1000

    Test the month

--- test/test_month.rb
+++ test/test_month.rb
@@ -8,18 +8,22 @@ context "#Budget::Month" do
 
   context "when 2014/12/22" do
     hookup { topic.calendar.day = Date.parse("2014/12/22") }
+    asserts("calendar.days.size") { topic.calendar.days.size }.equals(31)
+    asserts("calendar.bonus_days.size") { topic.calendar.bonus_days.size }.equals(4)
     asserts("cash") { topic.cash }.equals(930)
     asserts("buffer") { topic.buffer }.equals(93)
     asserts("bonus") { topic.bonus }.equals(240)
     asserts("total") { topic.total }.equals(1263)
   end
 
-  context "when 2016/03/15" do
-    hookup { topic.calendar.day = Date.parse("2016/03/15") }
-    asserts("cash") { topic.cash }.equals(930)
-    asserts("buffer") { topic.buffer }.equals(93)
+  context "when 2016/02/15" do
+    hookup { topic.calendar.day = Date.parse("2016/02/15") }
+    asserts("calendar.days.size") { topic.calendar.days.size }.equals(29)
+    asserts("calendar.bonus_days.size") { topic.calendar.bonus_days.size }.equals(4)
+    asserts("cash") { topic.cash }.equals(870)
+    asserts("buffer") { topic.buffer }.equals(87)
     asserts("bonus") { topic.bonus }.equals(240)
-    asserts("total") { topic.total }.equals(1263)
+    asserts("total") { topic.total }.equals(1197)
   end
 
 end
